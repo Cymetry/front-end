dd66507c0ac7c77aeb91f910704c868424774b59
Narek Ghevandiani <narghev@protonmail.com>
2020-03-28 16:26:10 +0400

    Add Tab Navigation Styles

--- umath/src/modules/content/index.js
+++ umath/src/modules/content/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Alert } from "react-native";
-import { withNavigation } from "react-navigation";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { Alert } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Learning from "./pages/learning";
-import Settings from "./pages/settings";
-import MyAccount from "./pages/my-account";
-import ROUTES from "../../platform/constants/routes";
+import Learning from './pages/learning';
+import Settings from './pages/settings';
+import MyAccount from './pages/my-account';
+import ROUTES from '../../platform/constants/routes';
 import {
+  navigationWrapper,
   createTabNavigationOptions,
-  navigationWrapper
-} from "../../platform/services/navigation";
+} from '../../platform/services/navigation';
 
 const Tab = createBottomTabNavigator();
 
@@ -20,20 +20,20 @@ const ContentScreens = ({ route }) => {
   const myAccountTabPressListener = {
     tabPress: event => {
       if (!loggedIn) {
-        Alert.alert("Please sign in to see your account", "", [
+        Alert.alert('Please sign in to see your account', '', [
           {
-            text: "Sign in",
-            onPress: () => navigationWrapper.navigation.navigate(ROUTES.AUTH)
+            text: 'Sign in',
+            onPress: () => navigationWrapper.navigation.navigate(ROUTES.AUTH),
           },
           {
-            text: "Cancel",
-            style: "cancel"
-          }
+            text: 'Cancel',
+            style: 'cancel',
+          },
         ]);
         event.preventDefault();
       }
       return null;
-    }
+    },
   };
 
   return (
@@ -44,20 +44,20 @@ const ContentScreens = ({ route }) => {
     >
       <Tab.Screen
         name={ROUTES.CONTENT_LEARNING}
-        options={createTabNavigationOptions("Learning", "sunny")}
+        options={createTabNavigationOptions('Learning', 'sunny')}
       >
         {props => <Learning {...props} id={id} />}
       </Tab.Screen>
       <Tab.Screen
-        listeners={myAccountTabPressListener}
-        name={ROUTES.CONTENT_MY_ACCOUNT}
         component={MyAccount}
-        options={createTabNavigationOptions("My Account", "person")}
+        name={ROUTES.CONTENT_MY_ACCOUNT}
+        listeners={myAccountTabPressListener}
+        options={createTabNavigationOptions('My Account', 'person')}
       />
       <Tab.Screen
-        name={ROUTES.CONTENT_SETTINGS}
         component={Settings}
-        options={createTabNavigationOptions("Settings", "settings")}
+        name={ROUTES.CONTENT_SETTINGS}
+        options={createTabNavigationOptions('Settings', 'settings')}
       />
     </Tab.Navigator>
   );
--- umath/src/platform/services/navigation.js
+++ umath/src/platform/services/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Platform } from '@unimodules/core';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Variables from '../../../assets/styles/variables';
 
@@ -10,29 +10,34 @@ export const createNavigationOptions = title => ({
 
 export const createTabNavigationOptions = (title, iconName) => ({
   tabBarLabel: title,
-  tabBarIcon: ({ tintColor }) => <Ionicons
-    name={Platform.OS === 'ios' ? `ios-${iconName}` : `md-${iconName}`}
-    size={24}
-    color={tintColor}
-  />,
+  tabBarIcon: ({ color }) => (
+    <Ionicons
+      size={24}
+      color={color}
+      name={Platform.OS === 'ios' ? `ios-${iconName}` : `md-${iconName}`}
+      style={{
+        marginTop: 5,
+      }}
+    />
+  ),
   tabBarOptions: {
-    inactiveTintColor: 'black',
     activeTintColor: Variables.blue,
+    inactiveTintColor: Variables.grey,
     style: {
       height: 60,
       paddingTop: 8,
       paddingBottom: 8,
-      alignItems: 'center',
       borderTopWidth: 1,
+      alignItems: 'center',
       borderTopColor: Variables.gray,
       backgroundColor: Variables.lightGray,
     },
     labelStyle: {
-      fontSize: 12,
-      padding: 0,
       margin: 0,
+      padding: 0,
+      fontSize: 12,
     },
-  }
+  },
 });
 
 // Would be set from home component

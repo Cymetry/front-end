c12515fc4b93051be00b8c747da17b437ebe3e3c
adsellor <adsellor@protonmail.com>
2020-04-03 14:07:51 +0400

    Restore mathjax as of commit [fab0ed8]

--- umath/src/components/math_jax.js
+++ umath/src/components/math_jax.js
@@ -35,7 +35,7 @@ class MathJax extends Component {
     else onMessage && onMessage(data);
   };
 
-  wrapMathjax(content) {
+  wrapMathjax = content => {
     const options = JSON.stringify(
       Object.assign({}, defaultOptions, this.props.mathJaxOptions)
     );
@@ -44,29 +44,26 @@ class MathJax extends Component {
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${options});
-
 				MathJax.Hub.Queue(function() {
 					var height = document.documentElement.scrollHeight;
-
-					window.postMessage(String(height));
+					window.ReactNativeWebView.postMessage(String(height));
 					Array.from(document.querySelectorAll("[id^='box-']")).map(function(item) {
 						item.onkeyup = item.keyup || function(e) {
 							const idNum = +e.target.id.replace('box-', '');
-							window.postMessage(JSON.stringify({ value: e.target.value, input: idNum - 1 }));
+							window.ReactNativeWebView.postMessage(JSON.stringify({ value: e.target.value, input: idNum - 1 }));
 						};
 					});
 				});
 			</script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
-
 			${content}
 		`;
-  }
+  };
 
   render() {
     const html = this.wrapMathjax(this.props.html);
-
     const props = Object.assign({}, this.props);
+
     delete props.html;
     delete props.webViewRef;
     delete props.onMessage;
@@ -76,7 +73,7 @@ class MathJax extends Component {
         <WebView
           originWhitelist={["*"]}
           scrollEnabled={true}
-          onLoad={this.handleMessage}
+          onMessage={this.handleMessage}
           useWebKit={true}
           javaScriptEnabled={true}
           source={{ html, baseUrl: "" }}

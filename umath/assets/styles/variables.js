0b8324cbbb64259d7c1b976b7cc3276d5842d631
narghev <narghev@protonmail.com>
2020-04-18 19:27:24 +0400

    fix global fonts for small size

--- umath/assets/styles/variables.js
+++ umath/assets/styles/variables.js
@@ -1,17 +1,21 @@
+import { PixelRatio } from "react-native";
+
+const PixelRatioValue = PixelRatio.get() <= 2;
+
 const Variables = {
-  titleFontSize: 32,
-  smallFontSize: 18,
-  normalFontSize: 22,
-  smallestFontSize: 16,
+  titleFontSize: PixelRatioValue ? 28 : 32,
+  smallFontSize: PixelRatioValue ? 14 : 18,
+  normalFontSize: PixelRatioValue ? 18 : 22,
+  smallestFontSize: PixelRatioValue ? 12 : 16,
 
   blue: "#0880FA",
   vividBlue: "#0091FF",
   lightBlue: "#32C5FF",
-  
-  darkGray: '#24272C',
+
+  darkGray: "#24272C",
   textGray: "#979797",
   gray: "#D8D8D8",
-  lightGray: "#F8F8F8"
+  lightGray: "#F8F8F8",
 };
 
 export default Variables;

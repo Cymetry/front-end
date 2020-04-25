131df340c1743d380725ed974f5661fd6de96192
narghev <narghev@protonmail.com>
2020-04-25 16:14:32 +0400

    separate skill testing item into new component;

--- umath/src/modules/content/pages/learning/pages/testing/index.js
+++ umath/src/modules/content/pages/learning/pages/testing/index.js
@@ -5,19 +5,13 @@ import { Button } from 'react-native-elements';
 import LocalStyles from './styles';
 import Styles from '../../../../../../../assets/styles';
 import DismissKeyboard from '../../../../../../components/dismiss-keyboard';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import MathJax from '../../../../../../components/math_jax';
-import { parseLatex } from '../../../../../../platform/services/latex';
 import TestingController from '../../../../../../platform/api/skillTesting';
-
-const exampleLatex = "$\\text{Find} \\text{one} \\text{solution} \\text{using} \\text{trial} \\text{and} \\text{error}$:$\\text{A} \\text{solution} \\text{is} x=\\FormInput[2]{box-1}.                                                       $";
-let scrollView = null;
+import TestingItem from './item';
 
 const Testing = ({ route }) => {
   const { id } = route.params;
   const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
 
   const startTesting = async id => {
     const result = await TestingController.Start(id);
@@ -32,40 +26,7 @@ const Testing = ({ route }) => {
     <View stlye={Styles.page}>
       <DismissKeyboard>
         <>
-          <KeyboardAwareScrollView
-            enableOnAndroid
-            keyboardShouldPersistTaps="handled"
-            innerRef={(ref) => (scrollView = ref)}
-            onPress={() => Keyboard.dismiss()}
-            onContentSizeChange={(width, height) =>
-              scrollView && scrollView.scrollTo({ y: height })
-            }
-          >
-            <View style={LocalStyles.container}>
-              <View style={LocalStyles.stepWrapper}>
-                <Text style={LocalStyles.stepText}>Question</Text>
-                <View style={LocalStyles.latexWrapper}>
-                  {
-                    questions[currentQuestion] && <MathJax
-                      html={parseLatex(questions[currentQuestion].question)}
-                    />
-                  }
-                </View>
-              </View>
-            </View>
-            <>
-              {
-                questions[currentQuestion] ? questions[currentQuestion].options.map((option, idx) => (
-                  <TouchableHighlight
-                    key={`${idx}-${option}`}
-                    onPress={() => setSelectedAnswer(idx)}
-                  >
-                    <Text style={idx === selectedAnswer ? { color: 'red' } : {}}>{option}</Text>
-                  </TouchableHighlight>
-                )) : null
-              }
-            </>
-          </KeyboardAwareScrollView>
+          <TestingItem question={questions[currentQuestion]} />
           <View style={LocalStyles.buttonWrapper}>
             <View
               style={{
---
+++ umath/src/modules/content/pages/learning/pages/testing/item.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableHighlight} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import MathJax from '../../../../../../components/math_jax';
+import { parseLatex } from '../../../../../../platform/services/latex';
+import LocalStyles from './styles';
+
+let scrollView = null;
+
+const TestingItem = ({ question }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  if (!question) return null;
+
+  return (
+    <KeyboardAwareScrollView
+      enableOnAndroid
+      keyboardShouldPersistTaps="handled"
+      innerRef={(ref) => (scrollView = ref)}
+      onPress={() => Keyboard.dismiss()}
+      onContentSizeChange={(width, height) =>
+        scrollView && scrollView.scrollTo({ y: height })
+      }
+    >
+      <View style={LocalStyles.container}>
+        <View style={LocalStyles.stepWrapper}>
+          <Text style={LocalStyles.stepText}>Question</Text>
+          <View style={LocalStyles.latexWrapper}>
+            {
+              <MathJax
+                html={parseLatex(question.question)}
+              />
+            }
+          </View>
+        </View>
+      </View>
+      <>
+        {
+          question.options.map((option, idx) => (
+            <TouchableHighlight
+              key={`${idx}-${option}`}
+              onPress={() => setSelectedAnswer(idx)}
+            >
+              <Text style={idx === selectedAnswer ? { color: 'red' } : {}}>{option}</Text>
+            </TouchableHighlight>
+          ))
+        }
+      </>
+    </KeyboardAwareScrollView>
+  );
+};
+
+export default TestingItem;
